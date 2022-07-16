382f29b2b3b3fe5924fd617324777c288d47291b
ANIRUDDHA SHINDE <52548052+anirudddh@users.noreply.github.com>
2022-07-16 15:14:46 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 dbPassword = 'mongodb+srv://Aniruddh:'+ encodeURIComponent('Aniruddh1321') + '@cluster0.mongodb.net/test?retryWrites=true';
 
 module.exports = {
-    mongodb+srv://Aniruddh:<Aniruddh1321>@cluster0.tjfkx.mongodb.net/?retryWrites=true&w=majority: Aniruddh1321
+    mongoURI: dbPassword
 };
